c46e59e86f7b6f6f993972690c7451b36cb2f9d8
Greatgod1-Ezekiel <greatgodeekiel123@gmail.com>
2026-08-17 17:55:48 -0700

    updated code

--- src/components/dashboard/Greeting.jsx
+++ src/components/dashboard/Greeting.jsx
@@ -5,7 +5,7 @@ function Greeting({ profile }) {
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-800">
-        Welcome, {firstName}! 👋
+        Welcome, {firstName}! 
       </h1>
 
       <p className="text-gray-500 mt-2">
