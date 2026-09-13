c98eb74d766e0e29ecf84560361daf30ca75cec5
Greatgod1-Ezekiel <greatgodeekiel123@gmail.com>
2026-09-13 15:28:43 -0700

    gg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,59 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true,
+      },
+
+      manifest: {
+        name: "CampusMart 2.0",
+        short_name: "CampusMart",
+        description:
+          "Shop products from verified campus sellers, post or apply for campus gigs, and enjoy secure Paystack payments.",
+        theme_color: "#008236",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        scope: "/",
+
+        start_url: "/app-start",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
   ],
-})
+});
